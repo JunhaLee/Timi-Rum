02023ee2ae2e48240303ebe9b929c9f96e0291d1
Daeyong96 <57346424+Daeyong96@users.noreply.github.com>
2021-01-20 17:36:14 +0900

    Update soundlevel.py

--- soundlevel.py
+++ soundlevel.py
@@ -2,12 +2,26 @@ import sounddevice as sd
 from numpy import linalg as LA
 import numpy as np
 import math
+import socket
+import time
 
+data=b'1'
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 duration = 10  # seconds
+noise_occur = 0
+cli = ("127.0.0.1",8000)
 
-def print_sound(indata, outdata, frames, time, status):
-    volume_norm = np.linalg.norm(indata)*10
-    print (int(volume_norm))
-
-with sd.Stream(callback=print_sound):
-    sd.sleep(duration * 500)
+while True:
+    def print_sound(indata, outdata, frames, time, status):
+        volume_norm = np.linalg.norm(indata)*10
+        if volume_norm > 80:
+            noise_occur = 1
+            print(volume_norm)
+            print("sound detect!!")
+            client_socket.sendto(data, cli)
+        else:
+            print("not detect...")
+            noise_occur = 0
+    
+    with sd.Stream(callback=print_sound):
+        sd.sleep(duration * 100)
