2f03681a9f3d37466a74b0e4a4cd0f14091b00fe
Sowon-Kim <72599000+Sowon-Kim@users.noreply.github.com>
2021-01-22 18:22:27 +0900

    Update sound_service.py

--- sound_service.py
+++ sound_service.py
@@ -1,5 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 15:59:25 2021
+
+@author: database
+"""
+
 import speech_recognition as sr
 import pyaudio
+import codecs
+import sys
+import pygame
 
 # 그냥 오디오 파일을 넣을 때
 # AUDIO_FILE = "hello.wav"
@@ -10,14 +20,22 @@ import pyaudio
 #     audio = r.record(source)  # 전체 audio file 읽기
 
 # microphone에서 auido source를 생성합니다
+pygame.mixer.init()
+sound = pygame.mixer.Sound("fall_check.wav")
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
+    sound.play()
     audio = r.listen(source)
 
 # 구글 웹 음성 API로 인식하기 (하루에 제한 50회)
 try:
-    print("Google Speech Recognition thinks you said : " + r.recognize_google(audio, language='ko'))
+    text = r.recognize_google(audio, language='ko')
+    print(text)
+    if (text == '응' or text == '그래' or text == '어' or text == '네'):
+        print(text + '<-')
+    elif (text == '아니' or text == '아니오' or text == '아냐' or text == '안돼'):
+        print(text + '<-')
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
